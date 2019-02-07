bfa4f5e603ad6f5945e384b6bfdbc4002b684660
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-02-07 13:13:10 -0700

    Add files via upload

---
+++ Ejercicio4_operaciones.java
@@ -0,0 +1,12 @@
+//Jose Luis De La Cruz Gonzalez 
+//Hora: 1:00 Dia 07/02/2019
+public class Ejercicio4_operaciones {
+ 
+ public static void main(String[] args) {
+  System.out.println(10 + 8 * 5);//imprime los numeros y operaciones de dicha fila
+  System.out.println((60+9) % 9);//imprime los numeros y operaciones de dicha fila
+  System.out.println(30 + -3*5 / 8);//imprime los numeros y operaciones de dicha fila
+  System.out.println(5 + 15 / 3 * 2 - 8 % 5);//imprime los numeros y operaciones de dicha fila
+ }
+ 
+}
