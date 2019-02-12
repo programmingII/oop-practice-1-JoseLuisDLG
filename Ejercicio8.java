c43aa5952e7445be49b79b8376d4f313ef8dfc0a
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-02-12 12:25:02 -0700

    Add files via upload

---
+++ Ejercicio8.java
@@ -0,0 +1,12 @@
+//José Luis De La Cruz Gonzalez 12-02-2019 12:16
+
+public class Ejercicio8{
+ 
+    public static void main(String[] args) {
+
+        System.out.println("   J    a   v     v  a ");//imprime la linea las letras que estan escritas
+        System.out.println("   J   a a   v   v  a a");//imprime la linea las letras que estan escritas	
+        System.out.println("J  J  aaaaa   V V  aaaaa");//imprime la linea las letras que estan escritas
+        System.out.println(" JJ  a     a   V  a     a");//imprime la linea las letras que estan escritas
+    }
+}
