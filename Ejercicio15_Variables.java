3d9722af564013a153c0ac8116b3f7ebab0d9631
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-02-12 23:37:21 -0700

    Add files via upload

---
+++ Ejercicio15_Variables.java
@@ -0,0 +1,15 @@
+//Jose Luis De La Cruz Gonzalez 12-02-2019 11:31 PM
+public class Ejercicio15_Variables {
+ 
+ public static void main(String[] args) {
+     
+   int a, b, temp;
+   a = 15;
+   b = 27;
+   System.out.println("Before swapping : a, b = "+a+", "+ + b);//imprime los valores de arriba y ademas las funciones y operaciones
+   temp = a;
+   a = b;
+   b = temp;   
+  System.out.println("After swapping : a, b = "+a+", "+ + b);//imprime los valores de arriba y ademas las funciones y operaciones
+ }
+ }
