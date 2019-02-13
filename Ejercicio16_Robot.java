ee7b0af27167311e5b2d92a27a4ccf5868f2e83f
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-02-13 14:27:53 -0700

    Add files via upload

---
+++ Ejercicio16_Robot.java
@@ -0,0 +1,11 @@
+//Jose Luis De La Cruz Gonzalez 13/02/2019 02:21 PM
+public class Ejercicio16_Robot {
+    public static void main(String[] args)
+    {
+        System.out.println(" +\"\"\"\"\"+ ");//Nos permite imprimir los caracteres de dicha fila
+        System.out.println("[| o o |]");//Nos permite imprimir los caracteres de dicha fila
+        System.out.println(" |  ^  | ");//Nos permite imprimir los caracteres de dicha fila
+        System.out.println(" | '-' | ");//Nos permite imprimir los caracteres de dicha fila
+        System.out.println(" +-----+ ");//Nos permite imprimir los caracteres de dicha fila
+    }
+	}
