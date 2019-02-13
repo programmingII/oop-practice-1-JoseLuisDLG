44658e9e8c8f8f10d134a60219570c0b0d910d03
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-02-12 19:57:17 -0700

    Add files via upload

---
+++ Ejercicio10_Formula.java
@@ -0,0 +1,9 @@
+//Jose Luis De La Cruz Gonzalez 12-02-2019 07:49 PM
+public class Ejercicio10_Formula {
+ 
+    public static void main(String[] args) {
+
+        double pi = 8.0 * (2 - (2.0/3) + (2.0/5) - (2.0/7) + (2.0/9) - (2.0/11));
+        System.out.println(pi); //imprime la linea de operaciones 
+    }
+}
