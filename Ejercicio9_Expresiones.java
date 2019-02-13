f919699a4de8a016cb9776aa69ff4b903778bed0
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-02-12 19:48:16 -0700

    Add files via upload

---
+++ Ejercicio9_Expresiones.java
@@ -0,0 +1,8 @@
+//Jose Luis De La Cruz Gonzalez 12-02-2019 07:28
+public class Ejercicio9_Expresiones {
+ 
+    public static void main(String[] arg) {
+
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));//Imprime las operaciones que se le ponen en dicha fila
+    }
+}
