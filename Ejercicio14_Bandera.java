629fa43add0d860bb05b4728a29bc3435a40590f
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-02-12 23:30:04 -0700

    Add files via upload

---
+++ Ejercicio14_Bandera.java
@@ -0,0 +1,22 @@
+//Jose Luis De La Cruz Gonzalez 12-02-2019 11:27 PM
+public class Ejercicio14_Bandera {
+ 
+   public static void main(String[] args)
+    {
+        System.out.println("* * * * * * ==================================");//imprime los caracteres de dicha fila
+        System.out.println(" * * * * *  ==================================");//imprime los caracteres de dicha fila
+        System.out.println("* * * * * * ==================================");//imprime los caracteres de dicha fila
+        System.out.println(" * * * * *  ==================================");//imprime los caracteres de dicha fila
+        System.out.println("* * * * * * ==================================");//imprime los caracteres de dicha fila
+        System.out.println(" * * * * *  ==================================");//imprime los caracteres de dicha fila
+        System.out.println("* * * * * * ==================================");//imprime los caracteres de dicha fila
+        System.out.println(" * * * * *  ==================================");//imprime los caracteres de dicha fila
+        System.out.println("* * * * * * ==================================");//imprime los caracteres de dicha fila
+        System.out.println("==============================================");//imprime los caracteres de dicha fila
+        System.out.println("==============================================");//imprime los caracteres de dicha fila
+        System.out.println("==============================================");//imprime los caracteres de dicha fila
+        System.out.println("==============================================");//imprime los caracteres de dicha fila
+        System.out.println("==============================================");//imprime los caracteres de dicha fila
+        System.out.println("==============================================");//imprime los caracteres de dicha fila
+    }
+}
