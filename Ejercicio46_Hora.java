6edec6fd051c8a87e439c7a1e8a73c577b7a5a7b
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-02-24 16:55:43 -0700

    Add files via upload

---
+++ Ejercicio46_Hora.java
@@ -0,0 +1,7 @@
+//Jose Luis De La Cruz Gonzalez 24-02-2019 04:51 PM
+public class Ejercicio46_Hora{
+  
+  public static void main(String[] args){
+         System.out.format("\nCurrent Date time: %tc%n\n", System.currentTimeMillis());//Este formato nos dice la hora de nuestro sistema en el momento que lo pedimos
+    }
+}
