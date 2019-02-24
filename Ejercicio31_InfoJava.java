3a5c7bac98da8e4b134707b026a48db08486890b
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-02-24 12:02:32 -0700

    Add files via upload

---
+++ Ejercicio31_InfoJava.java
@@ -0,0 +1,12 @@
+//Jose Luis De La Cruz Gonzalez 24-02-2019 11:54 AM
+public class Ejercicio31_InfoJava{
+public static void main(String[] args) {
+    System.out.println("\nJava Version: "+System.getProperty("java.version"));//Nos imprime la version de java
+    System.out.println("Java Runtime Version: "+System.getProperty("java.runtime.version"));//Nos imprime la runtime version de java
+    System.out.println("Java Home: "+System.getProperty("java.home"));//Nos imprime java home
+    System.out.println("Java Vendor: "+System.getProperty("java.vendor"));//Nos imprime java vendor
+    System.out.println("Java Vendor URL: "+System.getProperty("java.vendor.url"));//Nos imprime java vendor URL
+    System.out.println("Java Class Path: "+System.getProperty("java.class.path")+"\n");//Nos imprime Java Class Path
+ 
+ }
+}
