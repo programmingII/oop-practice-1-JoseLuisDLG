e6380f55b94fab36ae0892dc7a692861fef2f37b
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-02-24 15:13:15 -0700

    Add files via upload

---
+++ Ejercicio41_ValorASCII.java
@@ -0,0 +1,8 @@
+//Jose Luis De La Cruz Gonzalez 24-02-2019 03:07 PM
+public class Ejercicio41_ValorASCII {
+
+	public static void main(String[] String) {
+		int chr = 'Z';//Declaramos las variables en entero de caracter z
+		System.out.println("El valor ASCII de Z es :"+chr);//Nos imprime el valor en ASCII de Z
+	}
+}
