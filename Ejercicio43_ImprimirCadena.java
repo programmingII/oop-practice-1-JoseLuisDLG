acdf7840a78eab688001a665ff079d3cf3810313
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-02-24 16:11:01 -0700

    Add files via upload

---
+++ Ejercicio43_ImprimirCadena.java
@@ -0,0 +1,8 @@
+//Jose Luis De La Cruz Gonzalez 24-02-2019 04:06 PM
+public class Ejercicio43_ImprimirCadena {
+	public static void main(String[] args) {
+	
+   System.out.println("\nTwinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!\n\n");//Nos imprime el texto que esta en dicha linea
+
+}
+}
