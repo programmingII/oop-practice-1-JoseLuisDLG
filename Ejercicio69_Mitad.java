1866171874f377e2e26a440f0df4b45f7c948cb5
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-03-03 21:56:21 -0700

    Add files via upload

---
+++ Ejercicio69_Mitad.java
@@ -0,0 +1,15 @@
+//Jose Luis De La Cruz Gonzalez 01-03-2019 4:19 PM
+public class Ejercicio69_Mitad
+{
+	
+		
+		public static void main(String[] ags)
+		{
+			String frase = "Python 3.0";//declaramos variables string
+			
+			System.out.println(frase.substring(0, frase.length()/2));
+			}
+			
+		
+
+}
