160270e5ee9a2b563d69cc15b79a63f6dfebcb7c
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-03-03 21:51:38 -0700

    Add files via upload

---
+++ Ejercicio68_3Letras.java
@@ -0,0 +1,15 @@
+//Jose Luis De La Cruz Gonzalez 01-03-2019 03:22 PM
+public class Ejercicio68_3Letras
+{
+	
+		
+		public static void main(String[] ags)
+		{
+			String frase = "Python 3.0";//declaramos variables en string
+			String letras = frase.substring(frase.length() - 3);
+			System.out.println(letras + letras + letras + letras);//imprimimos el resultado
+		}
+			
+		
+
+}
