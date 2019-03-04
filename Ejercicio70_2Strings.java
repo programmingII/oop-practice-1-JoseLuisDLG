0ecba3a8f604e4b26277243a1533c3943ee76a41
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-03-03 21:59:59 -0700

    Add files via upload

---
+++ Ejercicio70_2Strings.java
@@ -0,0 +1,22 @@
+//Jose Luis De La Cruz Gonzalez 03-03-2019 09:43 PM
+public class Ejercicio70_2Strings
+{
+	
+		
+		public static void main(String[] ags)
+		{
+			String frase = "Python 3.0";//Declaramos la variable en string
+			String frase2 = "Tutorial";//Declaramos la variable en string
+			if(frase.length() >= frase2.length())
+			{//Creamos una condicional para saber si la primera frase es mayor o igual a la segunda
+			System.out.println( frase2+frase+frase2);
+			}
+			else
+			{
+			System.out.println(frase+frase2+frase);	
+			}
+		}
+			
+		
+
+}
