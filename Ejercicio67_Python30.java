00ab8ea8b0791d965ffc18cc22c36ab202ada7e7
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-03-03 21:47:01 -0700

    Add files via upload

---
+++ Ejercicio67_Python30.java
@@ -0,0 +1,10 @@
+//Jose Luis De La Cruz Gonzalez 01-03-2019 03:00 PM
+import java.lang.*;//Nos permite importar los tipos primitivos
+ public class Ejercicio67_Python30 {
+ public static void main(String[] args)
+ {
+   String main_string = "Python 3.0";//Nos permite declarar en string
+   String word = "Tutorial";//Nos imprime el tutorial de Python
+   System.out.println(main_string.substring(0, 7) + word + main_string.substring(6));//Nos imprime sustraer parte de la informacion de python
+	}
+}
